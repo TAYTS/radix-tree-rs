f483ff2377c57639ae3e301ebad32e2d847f32a4
TAYTS <taytzushieh@gmail.com>
2025-11-04 22:14:46 +0800

    feat: add method to create transaction on tree

--- src/tree.rs
+++ src/tree.rs
@@ -2,13 +2,17 @@
 
 mod transaction;
 
-use crate::{node::Node, utils::NodeValue};
+use std::sync::Arc;
+
+use parking_lot::lock_api::RwLock;
+
+use crate::{node::Node, tree::transaction::Txn, utils::NodeValue};
 
 pub struct Tree<T>
 where
     T: NodeValue,
 {
-    root: Node<T>,
+    root: Arc<Node<T>>,
     size: u32,
 }
 
@@ -19,5 +23,15 @@ where
     Tree {
         root: Node::default().into(),
         size: 0,
+    }
+}
+
+impl<T: NodeValue> Tree<T> {
+    pub fn transaction(&self) -> Txn<T> {
+        Txn {
+            root: RwLock::new(self.root.clone()),
+            size: self.size.into(),
+            writable: None,
+        }
     }
 }
