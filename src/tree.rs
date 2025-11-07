94d33628498443935f62ba048b580f2f398335a6
TAYTS <taytzushieh@gmail.com>
2025-11-07 22:10:20 +0800

    feat: add public method to tree

--- src/tree.rs
+++ src/tree.rs
@@ -27,11 +27,51 @@ where
 }
 
 impl<T: NodeValue> Tree<T> {
+    /// Get the number of node in the tree.
+    pub fn len(&self) -> u32 {
+        self.size
+    }
+
+    /// Get the root node of the tree.
+    pub fn root(&self) -> Arc<Node<T>> {
+        self.root.clone()
+    }
+
+    /// Get the value associated with the given key if exists.
+    pub fn get(&self, key: &str) -> Option<T> {
+        self.root.get(key)
+    }
+
+    /// Create a new transaction for the tree.
     pub fn transaction(&self) -> Txn<T> {
         Txn {
             root: RwLock::new(self.root.clone()),
             size: self.size.into(),
             writable: None,
         }
+    }
+
+    /// Insert a key-value pair into the tree, returning the new tree and the old value if exists.
+    pub fn insert(&self, key: &str, value: T) -> (Tree<T>, Option<T>) {
+        let mut txn = self.transaction();
+        let old_value = txn.insert(key, value);
+        let new_tree = txn.commit();
+        (new_tree, old_value)
+    }
+
+    /// Delete a key from the tree, returning the new tree and the old value if exists.
+    pub fn delete(&self, key: &str) -> (Tree<T>, Option<T>) {
+        let mut txn = self.transaction();
+        let old_value = txn.delete(key);
+        let new_tree = txn.commit();
+        (new_tree, old_value)
+    }
+
+    /// Delete all keys with the given prefix from the tree, returning the new tree and a boolean indicating if any keys were deleted.
+    pub fn delete_prefix(&self, prefix: &str) -> (Tree<T>, bool) {
+        let mut txn = self.transaction();
+        let has_deleted = txn.delete_prefix(prefix);
+        let new_tree = txn.commit();
+        (new_tree, has_deleted)
     }
 }
